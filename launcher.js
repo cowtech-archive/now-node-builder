787fef30c38def7e69841a0c5c11b6c748f6f58d
Shogun <shogun@cowtech.it>
2018-11-11 10:07:09 +0100

    feat: Use a better launcher for Node.js.

--- launcher.js
+++ launcher.js
@@ -2,7 +2,43 @@ const fs = require('fs')
 const workPath = `${process.cwd()}/user`
 const entrypoint = `${workPath}/{{ENTRYPOINT}}`
 
+// Setup some environment variables
 process.env.NODE_ENV = 'production'
 process.env.NOW_DEPLOYMENT = 'true'
 process.chdir(workPath)
-module.exports.launcher = require(entrypoint)
+
+// Start the main function
+require(entrypoint)
+
+module.exports.launcher = async function(event) {
+  // Parse the event in order to obtain all the required informations, supports both forms used by now
+  if (event.Action === 'Invoke') event = JSON.parse(event.body)
+
+  // Get all parameters
+  const { httpMethod, path, headers, encoding } = event
+
+  // Normalize method and body since they can come from two event types
+  let { method, body } = event
+  if (!method) method = httpMethod
+  if (encoding === 'base64') body = Buffer.from(body, encoding)
+
+  // Forward the request to localhost
+  try {
+    const response = await got(`http://127.0.0.1:${port}${path}`, { method, headers, body, throwHttpErrors: false })
+
+    // Adjust some response parameters
+    delete response.headers.connection
+    delete response.headers['content-length']
+    if (typeof response.body === 'object') response.body = JSON.stringify(response.body)
+
+    // Return the response of the event
+    return {
+      statusCode: response.statusCode,
+      headers: response.headers,
+      body: Buffer.from(response.body).toString('base64'),
+      encoding: 'base64'
+    }
+  } catch (e) {
+    throw e
+  }
+}
